1ed1d04acb400f65b98bf137025fd0f323b17642
Raymond L Maple <rmaple@neumont.edu>
2026-04-08 12:38:58 -0600

    class 040826

--- src/body.cpp
+++ src/body.cpp
@@ -1,11 +1,30 @@
 #include "body.h"
 
-void Body::AddForce(Vector2 force)
+void Body::AddForce(Vector2 force, ForceMode forceMode)
 {
-	acceleration += force / mass;
+	if (bodyType != BodyType::Dynamic) return;
+
+	switch (forceMode)
+	{
+	case ForceMode::Force:
+		acceleration += force * inverseMass;
+		break;
+	case ForceMode::Impulse:
+		velocity += force * inverseMass;
+		break;
+	case ForceMode::Acceleration:
+		acceleration += force;
+		break;
+	case ForceMode::VelocityChange:
+		velocity += force;
+		break;
+	default:
+		break;
+	}
 }
 
 void Body::Draw() const
 {
 	DrawCircleV(position, size, RED);
+	DrawCircleLinesV(position, size, WHITE);
 }
--- src/body.h
+++ src/body.h
@@ -2,17 +2,34 @@
 #include "raylib.h"
 #include "raymath.h"
 
+enum class BodyType
+{
+	Static,
+	Kinematic,
+	Dynamic
+};
+
+enum class ForceMode
+{
+	Force,
+	Impulse,
+	Acceleration,
+	VelocityChange
+};
+
 struct Body
 {
-	Vector2 position;
-	Vector2 velocity;
-	Vector2 acceleration;
+	BodyType bodyType = BodyType::Dynamic;
+	Vector2 position{ 0, 0 };
+	Vector2 velocity{ 0, 0 };
+	Vector2 acceleration{ 0, 0 };
 	float mass = 1.0f;
+	float inverseMass = 1.0f; // 1 / mass
 	float size = 1.0f;
 	float restitution = 1.0f;
 	float gravityScale = 1.0f;
 	float damping = 0.0f;
 
-	void AddForce(Vector2 force);
+	void AddForce(Vector2 force, ForceMode forceMode = ForceMode::Force);
 	void Draw() const;
 };
